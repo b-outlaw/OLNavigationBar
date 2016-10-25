36dd1406bd8f540c92b75da07a4a848bb7d7b971
b-outlaw <boutlaw@goldstar.com>
2016-10-25 14:41:23 -0700

    Adding a Navigation Controller to change background color of the nav bar.

---
+++ App/OLNavigationController.swift
@@ -0,0 +1,21 @@
+//
+//  OLNavigationController.swift
+//  OLScrollingNavBar
+//
+//  Created by Brandon Outlaw on 10/24/16.
+//  Copyright © 2016 Goldstar Events, Inc. All rights reserved.
+//
+
+import UIKit
+
+class OLNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
